ccdc6d5d35e2f055d6df9d550a73a3af68bd1ecb
sixyouss <youcef.zahariou@gmail.com>
2019-02-23 06:46:26 +0100

    working on section 5

--- src/components/Section5/Section5.jsx
+++ src/components/Section5/Section5.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Section5.css";
 import greenBean from "./images/obj_03.png";
+import prodPic from "../Section3/images/prod.png";
+import woman from "./images/women_tea.png";
+import checkPic from "../Section1/images/check_03.png";
 const Section5 = () => {
   return (
     <section className="section5">
@@ -25,8 +28,66 @@ const Section5 = () => {
           </h4>
           <span>استمتع بخسارة الوزن مع مذاق جيد!</span>
         </div>
+        <div>
+          <h3>ناولها باستمرار لنتائج تدوم لفترة طويلة.</h3>
+          <p>
+            يجب تناول Café Vert بانتظام لخسارة الوزن الزائد وتعود دومًا إلى شكلك
+            المثالي.
+            <h3>كيف يساعدك Café Vert في خسارة وزنك؟</h3>
+            يساعدك Café Vert في خسارة وزنك بتعزيز التمثيل الغذائي مع حمض
+            الكلوروجينيك الموجود في محتواها.
+            <br />
+            حمض الكلوروجينيك، يُنصح بشدة للذين يريدون خسارة الوزن، كما يقي الجسم
+            ضد عدد كبير من الأمراض الأيضية المزمنة مثل السكر، وأمراض القلب
+            والأوعية الدموية، والسرطان، وكذلك بعض الاضطرابات العصبية الخطيرة بما
+            في ذلك مرض الزهايمر ومرض باركنسون.
+          </p>
+        </div>
       </div>
-      <div className="section5-right" />
+      <div className="section5-right">
+        <div className="section5-right-woman">
+          <img src={woman} alt="woman tea" />
+          <div>
+            <h3>عبير</h3>
+            <h2>النحافة!</h2>
+            <h4>جرّب الآن!</h4>
+            <p>
+              ابدأ يومك مع فنجان Café Vert قبل 15 دقيقة من وجبة الإفطار. إذا كنت
+              ترغب في ذلك، يُمكنك تحضير Café Vert على الطريقة التركية.
+            </p>
+          </div>
+        </div>
+
+        <div  className="section5-right-bg">
+          <div className="section5-right-titles">
+            <div>
+              <img src={checkPic} alt="check" />
+              <p>يحرق الدهون. </p>
+            </div>
+            <div>
+              <img src={checkPic} alt="check" />
+              <p>يساعد في خسارة الوزن. </p>
+            </div>
+            <div>
+              <img src={checkPic} alt="check" />
+              <p>يقي من الأمراض. </p>
+            </div>
+            <div>
+              <img src={checkPic} alt="check" />
+              <p>ينظم التمثيل الغذائي. </p>
+            </div>
+          </div>
+          <div className="section5-right-prod">
+            <div className="section5-right-prod-price neucha">
+              <p>3990 DZD</p>
+              <button className="order-button-form">اطلب الآن</button>
+            </div>
+            <div className="section5-right-prod-img">
+              <img src={prodPic} alt="product" />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
